26bb052f6ba0c9c11bc0c7be7f80ac93315f4332
Connor <mussonconnor@gmail.com>
2023-08-03 10:31:24 +0100

    commit to rebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Game from "./components/Game";
 import Score from "./components/Score";
+import Player from "./components/Player";
 
 function App() {
   const [starter, setStarter] = useState(true);
@@ -28,32 +29,12 @@ function App() {
   return (
     <div className="app">
       <div className="title">
-        <img
-          className="title-img"
-          src="https://www.lobby.so/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fcomponents%2Flayout%2Flogo.b8a48fd7046ed4ca38ec70f9a8717100.png&w=96&q=75"
-          alt="logo"
-        />
+        <img className="title-img" src="/player1.jpeg" alt="logo" />
         <h1> Connect 4</h1>
       </div>
 
       <div className="main-container">
-        <div className="player">
-          <h4>Current Player</h4>
-          {currentPlayer === "a" ? (
-            <img
-              className="current"
-              src="https://www.lobby.so/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fcomponents%2Flayout%2Flogo.b8a48fd7046ed4ca38ec70f9a8717100.png&w=96&q=75"
-              alt="logo"
-            />
-          ) : (
-            <img
-              className="current"
-              src="https://wiki.lobby.so/icon.svg "
-              alt="logo"
-            />
-          )}
-        </div>
-
+        <Player currentPlayer={currentPlayer} />
         <Game
           currentPlayer={currentPlayer}
           setCurrentPlayer={setCurrentPlayer}
